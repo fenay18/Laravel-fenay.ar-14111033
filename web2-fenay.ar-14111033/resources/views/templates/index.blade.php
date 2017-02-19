fd2bcd50385b627a09c462a05161612882746f42
fenay18 <fennyanggrae18@gmail.com>
2017-02-19 19:26:41 +0700

    UTSLaravel

---
+++ web2-fenay.ar-14111033/resources/views/templates/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Latihan Blade Template</title>
+</head>
+<body>
+@include('templates.header')
+@yield('content')
+@include('templates.footer')
+</body>
+</html>
